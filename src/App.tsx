3da03f93e55bd2c98ce34745c509f12797f92b57
Laura-Blanco <60363157+Laura-Blanco@users.noreply.github.com>
2025-07-01 00:26:17 -0600

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Hero from './components/Hero'
 import Details from './components/Details'
 import Gift from './components/Gift'
