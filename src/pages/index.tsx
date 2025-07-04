4e27f49b4a64ede699487614bc9a066f1e4653e7
Laura-Blanco <60363157+Laura-Blanco@users.noreply.github.com>
2025-07-04 16:37:34 -0600

    add favicon and titles for message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,6 +29,36 @@ export default function Home() {
       <Head>
         <title>Dilean + Jeremy</title>
         <link rel="icon" href="/assets/favicon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+
+        <meta property="og:url" content="https://dilean-jeremy.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Boda de Dilean & Jeremy"/>
+        {/* <meta
+          property="og:description"
+          content="Join us for our special day on August 15, 2025"
+        /> */}
+        <meta
+          property="og:image"
+          content="https://dilean-jeremy.vercel.app/assets/dj.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Boda de Dilean & Jeremy"/>
+        {/* <meta
+          name="twitter:description"
+          content="Join us for our special day on August 15, 2025"
+        /> */}
+        <meta
+          name="twitter:image"
+          content="https://dilean-jeremy.vercel.app/assets/dj.png"
+        />
+
       </Head>
 
       {isMobile ? (
